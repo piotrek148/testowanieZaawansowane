66a1fa69dde435b47cc11ae0d2507580a8613fe7
Piotr Kukla <ptrkukla148@gmail.com>
2020-10-25 10:35:06 +0100

    clean app

---
+++ src/main/java/pl/sda/bootcamp/BootcampSdaApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/bootcamp/BootcampSdaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
